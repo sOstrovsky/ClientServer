86370893915ed6ff23eceff67a9a6c1fb99143ba
Sergey Ostrovsky <sergey.ostrovsky@nordigy.ru>
2022-05-05 23:44:11 +0400

    [task1] try with resources added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Server started");
         int port = 8090;
 
-        ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("New connection accepted");
 
-            System.out.println("New connection accepted");
-
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, your port id %d", name, clientSocket.getPort()));
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port id %d", name, clientSocket.getPort()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
